d1c240c969e5b4c1a5aede8b90655876ccddd3f4
Michael Burns <michael@mirwin.net>
2016-10-21 16:35:39 -0700

    add test basics

---
+++ test/unit/spec/default_spec.rb
@@ -0,0 +1,24 @@
+require 'chefspec'
+require_relative 'spec_helper'
+
+describe 'foobar::default' do
+  before { stub_resources }
+
+  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+
+  it 'creates group' do
+    expect(chef_run).to create_group('treslek')
+  end
+  
+  it 'creates directory' do
+    expect(chef_run).to create_directory('/usr/local/treslek')
+  end
+  
+  it 'creates config file' do
+    expect(chef_run).to create_template('/etc/treslek/config.json')
+  end
+
+  it 'creates config file' do
+    expect(chef_run).to create_template('/usr/local/treslek//plugins/treslek-gh-issue-search/config.json')
+  end  
+end
