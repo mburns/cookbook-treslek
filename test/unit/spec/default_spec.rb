c9a34d02ac7ff8bc8cb9355041e7f7ccc5205c36
Michael Burns <michael@mirwin.net>
2016-11-08 16:03:52 -0800

    fix cookbook name

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -1,7 +1,7 @@
 require 'chefspec'
 require_relative 'spec_helper'
 
-describe 'foobar::default' do
+describe 'treslek::default' do
   before { stub_resources }
 
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
